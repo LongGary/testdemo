87490c80299f9c489850f95dfb0b9f707aa34461
longjiali <414495467@qq.com>
2020-03-14 10:37:19 +0800

    unittest

--- test/test001.py
+++ test/test001.py
@@ -1,7 +1,13 @@
+import unittest
+from unittest import TestCase
 
 
-
-class Test666:
+class Test666(unittest.TestCase):
     def test888(self):
        for i in range(10):
            print("我成功啦",i)
+
+
+if __name__ == '__main__':
+    hello=Test666()
+    hello.test888()
